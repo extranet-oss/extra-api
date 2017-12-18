12e5aeb25eeb00b540dcb9314ef8291c35dc5394
Jordan Demaison <jordan.demaison@gmail.com>
2017-12-18 03:35:19 +0100

    Added azuread auth strategy

--- src/authentication.js
+++ src/authentication.js
@@ -1,7 +1,9 @@
 const authentication = require('feathers-authentication');
 const jwt = require('feathers-authentication-jwt');
 const AuthTokenStrategy = require('passport-auth-token').Strategy;
-
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const session = require('express-session');
+const querystring = require('querystring');
 
 
 module.exports = function () {
@@ -25,10 +27,55 @@ module.exports = function () {
     }
   ));
 
+  app.passport.use('azuread', new OIDCStrategy(config.azuread,
+    function(iss, sub, profile, accessToken, refreshToken, done) {
+      if (!profile.oid) {
+        return done(new Error("No oid found"), null);
+      }
+
+      // Todo : login user from upn
+      done(null, {});
+    }
+  ));
+
+  const sessionMiddleware = session({secret: config.secret});
+
+  app.get('/auth/azuread',
+    sessionMiddleware,
+    authentication.express.authenticate('azuread', {
+      failureRedirect: '/error',
+      session:false
+    })
+  );
+
+  app.get('/auth/azuread/callback',
+    sessionMiddleware,
+    authentication.express.authenticate('azuread', {
+      failureRedirect: '/error',
+      session:false
+    }),
+    function(req, res) {
+      const data = {};
+      const params = {};
+
+      // Todo : passing the right payload to authenticate service
+      app.service(config.path).create(data, params).then(result => {
+        res.data = result;
+
+        // Todo : setting success callback or something
+        res.redirect('/?' + querystring.stringify(result));
+      }).catch(() => {
+
+        // Todo : setting error callback or something
+        res.redirect('/error');
+      });
+    }
+  );
+
   // The `authentication` service is used to create a JWT.
   // The before `create` hook registers strategies that can be used
   // to create a new valid JWT (e.g. local or oauth2)
-  app.service('authentication').hooks({
+  app.service(config.path).hooks({
     before: {
       create: [
         authentication.hooks.authenticate(config.strategies)
