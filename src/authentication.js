8fc10f91fdfde9a1efc2b29698ec5aec2c64119e
Jordan Demaison <jordan.demaison@gmail.com>
2017-12-19 00:27:33 +0100

    azuread login flow now handles correctly redirections to front callback url

--- src/authentication.js
+++ src/authentication.js
@@ -46,18 +46,35 @@ module.exports = function () {
 
   app.get('/auth/azuread',
     sessionMiddleware,
-    authentication.express.authenticate('azuread', {
-      failureRedirect: '/error',
-      session:false
-    })
+    (req, res, next) => {
+      // register redirect_uri in session
+      let redirect_uri;
+      if (req.query.hasOwnProperty("redirect_uri") && config.azuread.returnURLs.find(x => x==req.query.redirect_uri))
+        redirect_uri = req.query.redirect_uri;
+      else
+        redirect_uri = config.azuread.returnURLs[0];
+
+      req.session.azuread_failureRedirect = `${redirect_uri}?success=false`;
+      req.session.azuread_successRedirect = `${redirect_uri}?success=true`;
+
+      authentication.express.authenticate('azuread', {
+        failureRedirect: req.session.azuread_failureRedirect,
+        session:false
+      })(req, res, next);
+    }
   );
 
   app.get('/auth/azuread/callback',
     sessionMiddleware,
-    authentication.express.authenticate('azuread', {
-      failureRedirect: '/error',
-      session:false
-    }),
+    (req, res, next) => {
+      if (!req.session.hasOwnProperty("azuread_failureRedirect") || !req.session.hasOwnProperty("azuread_successRedirect"))
+        res.redirect(`${config.azuread.returnURLs[0]}?success=false`);
+
+      authentication.express.authenticate('azuread', {
+        failureRedirect: req.session.azuread_failureRedirect,
+        session:false
+      })(req, res, next);
+    },
     function(req, res) {
       const data = {};
       const params = {};
@@ -67,11 +84,11 @@ module.exports = function () {
         res.data = result;
 
         // Todo : setting success callback or something
-        res.redirect('/?' + querystring.stringify(result));
+        res.redirect(`${req.session.azuread_successRedirect}&${querystring.stringify(result)}`);
       }).catch(() => {
 
         // Todo : setting error callback or something
-        res.redirect('/error');
+        res.redirect(req.session.azuread_failureRedirect);
       });
     }
   );
