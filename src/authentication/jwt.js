e438f696fd4d721c3950315be39f78835ffef102
Jordan Demaison <Jordan.Demaison@gmail.com>
2018-03-21 13:02:33 +0100

    Added custom JWT verifier

--- src/authentication/jwt.js
+++ src/authentication/jwt.js
@@ -5,11 +5,6 @@ class CustomVerifier {
   constructor (app, options = {}) {
     this.app = app;
     this.options = options;
-    this.service = typeof options.service === 'string' ? app.service(options.service) : options.service;
-
-    if (!this.service) {
-      throw new Error('options.service does not exist.\n\tMake sure you are passing a valid service path or service instance and it is initialized before @feathersjs/authentication-jwt.');
-    }
 
     this.verify = this.verify.bind(this);
   }
@@ -20,20 +15,106 @@ class CustomVerifier {
 
     // the 'user' variable can be any truthy value
     // the 'payload' is the payload for the JWT access token that is generated after successful authentication
-    //console.log(payload);
-    const id = payload[`${this.options.entity}Id`];
 
-    if (id === undefined) {
-      return done(null, payload.sub, {});
+    // check token validity & infos
+    if (!payload.ver || payload.ver != 1) {
+      return done(null, false);
     }
 
-    this.service.get(id).then(entity => {
-      const newPayload = { [`${this.options.entity}Id`]: id };
-      return done(null, entity, newPayload);
+    if (!payload.sub || payload.sub != 'extra-api:access_token') {
+      return done(null, false);
+    }
+
+    if (!payload.client || !payload.scopes) {
+      return done(null, false);
+    }
+
+    // Define default results
+    var new_payload = {
+          sub: 'extra-api:access_token',
+          client: null,
+          user: null,
+          scopes: []
+        },
+        result = {
+          client: null,
+          user: null,
+          authorization: null,
+          scopes: []
+        };
+
+    // Load services
+    const clients = this.app.service('oauth/clients');
+    const authorizations = this.app.service('oauth/authorizations');
+    const users = this.app.service('users');
+
+    // Set scopes
+    result.scopes = payload.scopes;
+    new_payload.scopes = payload.scopes;
+
+    // Process client
+    clients.find({
+      query: {
+        id: payload.client.id,
+        validity_token: payload.client.validity
+      }
     })
-      .catch(error => {
-        return done(error);
-      });
+    .then(matches => {
+      if (matches.total == 0)
+        return done(null, false);
+
+      // Save client
+      result.client = matches.data[0];
+      new_payload.client = {
+        id: result.client.id,
+        validity: result.client.validity_token
+      };
+
+      // If associated user, process user
+      if (payload.user) {
+        users.find({
+          query: { id: payload.user.id }
+        })
+        .then(matches => {
+          if (matches.total == 0)
+            return done(null, false);
+
+          // Save user
+          result.user = matches.data[0];
+          new_payload.user = {
+            id: result.user.id,
+            authorization: null
+          };
+
+          // deny login if user is suspended
+          if (result.user.suspended)
+            return done(null, false, `Account suspended: ${result.user.suspended_reason ? result.user.suspended_reason : 'no info'}`)
+
+          // Check if authorization is valid
+          authorizations.find({
+            query: { id: payload.user.authorization }
+          })
+          .then(matches => {
+            if (matches.total == 0)
+              return done(null, false);
+
+            // Save authorization
+            result.authorization = matches.data[0];
+            new_payload.user.authorization = result.authorization.id;
+
+            // finalize
+            done(null, result, new_payload);
+          })
+          .catch(err => done(err));
+        })
+        .catch(err => done(err));
+      }
+      else {
+        done(null, result, new_payload);
+      }
+    })
+    .catch(err => done(err));
+
   }
 }
 
